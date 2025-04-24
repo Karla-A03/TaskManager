d50d1f597b10d8c115334594a7aa608d25527522
Alejandro Almanza López <alejandroalmanzalopez@gmail.com>
2025-04-23 22:17:25 -0600

    Addying view files

--- TaskManagerApp/View/MainWindow.xaml.cs
+++ TaskManagerApp/View/MainWindow.xaml.cs
@@ -8,17 +8,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TaskManagerApp.Controller;
+using Task = TaskManagerApp.Model.Task;
 
 namespace TaskManagerApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TaskController _controller;
+
         public MainWindow()
         {
             InitializeComponent();
+            _controller = new TaskController(this);
+
+            // Evento para filtrar tareas por categoría
+            cmbFiltroCategoria.SelectionChanged += CmbFiltroCategoria_SelectionChanged;
+
+            // Event Handlers para marcar tareas y eliminar tareas
+            btnMarcarCompletada.Click += BtnMarcarCompletada_Click;
+            btnEliminarTarea.Click += BtnEliminarTarea_Click;
         }
+
+        private void CmbFiltroCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var categoriaSeleccionada = (cmbFiltroCategoria.SelectedItem as ComboBoxItem)?.Content?.ToString();
+            if (!string.IsNullOrEmpty(categoriaSeleccionada))
+            {
+                _controller.FiltrarPorCategoria(categoriaSeleccionada);
+            }
+        }
+
+        private void BtnMarcarCompletada_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvTareas.SelectedItem is Task tareaSeleccionada)
+            {
+                _controller.MarcarTareaComoCompletada(tareaSeleccionada);
+            }
+        }
+
+        private void BtnEliminarTarea_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvTareas.SelectedItem is Task tareaSeleccionada)
+            {
+                _controller.EliminarTarea(tareaSeleccionada);
+            }
+        }
+
     }
 }
